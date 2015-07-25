80389ed0b140587d12cf3b185135f7782b72f478
Tikitoo <codes0306@gmail.com>
2015-07-25 12:31:11 +0800

    first commit

---
+++ app/src/main/java/com/tikitoo/slidelayout/MainActivity.java
@@ -0,0 +1,81 @@
+package com.tikitoo.slidelayout;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+/**
+ * 参考：https://github.com/lzyzsd/SwipeLayout
+ */
+public class MainActivity extends AppCompatActivity {
+    private ListView mListview;
+    private Context mCtx;
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCtx = this;
+        initViews();
+    }
+
+    private void initViews() {
+        mListview = (ListView) findViewById(R.id.list_view);
+        adapter = new MyAdapter();
+        mListview.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 100;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup viewGroup) {
+            if (convertView == null) {
+                convertView = new MoveLayout(mCtx);
+            }
+            return convertView;
+        }
+    }
+}
